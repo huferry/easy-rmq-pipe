17a592188ae952db4ac7c39801055eae910493d0
LAPTOP-C284JHA0\hufer <huferry@gmail.com>
2019-11-08 20:12:36 +0100

    add onlog

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const rmq = require('easy-rmq')
 
-const process = (handler, nextQueue, onError) => (payload) => {
+const process = (handler, nextQueue, onError, _emitLogs) => (payload) => {
     const r = handler(payload)
     if (nextQueue) {
-        if (r && r.then) r.then(nextPayload =>
+        if (r && r.then) r.then(nextPayload => {
+            _emitLogs({text:'Messaged processed.', payload})
             nextQueue.publish(nextPayload || payload)
-        ).catch(e => onError(e, payload))
-        else nextQueue.publish(r || payload)
+        }).catch(e => onError(e, payload))
+        else {
+            _emitLogs({text:'Messaged processed.', payload})
+            nextQueue.publish(r || payload)
+        }
     }
 }
 
@@ -19,10 +23,19 @@ const createQueueName = (settings, givenName) => {
 
 const create = (settings) => {    
 
+    const logListeners = []
+    const _emitLogs = (log) => {
+        if (typeof log === 'string') return _emitLogs({text: log})
+        log.timestamp = new Date()
+        logListeners.forEach(l => l(log))
+    }
+
+
     const chain = []
     settings.pipelineName = settings.pipelineName || `pipeline-${tick()}`
 
     const pipeline = {}
+
     pipeline.pipe = (handler, queueName, onError) => {
         chain.push({handler, queueName: createQueueName(settings, queueName), onError})
         return pipeline
@@ -35,22 +48,39 @@ const create = (settings) => {
             c.queue = await conn.queue(c.queueName)
         }))
 
-        chain.forEach(({handler, queue, onError}, idx) => {
+        chain.forEach(({handler, queue, onError, queueName}, idx) => {
             const next = chain[idx+1]
             const nextQueue = next ? next.queue : undefined
-            queue.subscribe(process(handler, nextQueue, (error, payload) => {
-                onError && onError(error, () => {
-                    queue.publish(payload)
-                })
-            }))
+            queue.subscribe(process(
+                handler, 
+                nextQueue,
+                (error, payload) => {
+                    onError && onError(error, (delay) => {
+                        const ms = delay ? delay : 1000
+                        _emitLogs({text:`Requeueing in ${ms}ms`, error})
+                        setTimeout(()=>
+                            queue.publish(payload),
+                            ms)
+                    })
+                },
+                (log) => {
+                    log.queueName = queueName
+                    _emitLogs(log)
+                }
+            ))
         })
+
+        _emitLogs(`Pipeline [${settings.pipelineName}] is ready`)
     }
 
     pipeline.insert = (payload, queueName) => {
         const c = queueName ? chain.filter(c => c.queueName === queueName)[0] : chain[0] 
         c.queue.publish(payload)
+        _emitLogs({text: `Messaged queued on [${c.queueName}]`, payload })
         return pipeline
     }
+
+    pipeline.onlog = (logListener) => logListeners.push(logListener)
 
     return pipeline
     
